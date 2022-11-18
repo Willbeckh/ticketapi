416a93ea74e07a15af9d879fa70b0992919e5591
Willbeckh <wileyhndirangu04@gmail.com>
2022-11-18 14:00:53 +0300

    patch:tokenization views

--- tickets/views.py
+++ tickets/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from rest_framework import routers, serializers, viewsets
-from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 from tickets.models import Ticket, Category
-from tickets.serializers import UserSerializer, TicketSerializer, CategorySerializer
+from tickets.serializers import *
+from django.contrib.auth.models import User
 
-# Create your views here.
+# Create your views here.        
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
     
     
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated] # require token authentication.
     
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+    serializer_class = MyTokenObtainPairSerializer
