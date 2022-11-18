9cf1f4836123237e5c97a9f73831436ebb9980de
Willbeckh <wileyhndirangu04@gmail.com>
2022-11-18 13:52:04 +0300

    feat:add ai auth/ url

--- ticketapi/urls.py
+++ ticketapi/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from tickets.models import Ticket, Category
 from rest_framework import routers
 from tickets.views import *
@@ -10,11 +11,11 @@ from tickets.views import *
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
 router.register(r'api/tickets', TicketViewSet)
-router.register(r'api/category', CategoryViewSet)
+router.register(r'api/category', CategoryViewSet, basename='Category')
 
 # wire up paths to router for automatic routing.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/', include('auth.urls')),
 ]
