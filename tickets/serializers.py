1ef37427855ef9c40c4b57b945f0df3787bdb271
Willbeckh <wileyhndirangu04@gmail.com>
2022-11-18 13:57:19 +0300

    chore:serialize api db models

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,9 +1,21 @@
 from .models import Ticket, Category
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 # write your model sserializers here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """This class customises the jwt token payload claims and adds username."""
+    @classmethod
+    def get_token(cls, user):
+        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+        
+        # add custom claims
+        token['username'] = user.username
+        return token
+    
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
