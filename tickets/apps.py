3101c897b8ee3268f8c3d3bc4478d08f471e43b5
Willbeckh <wileyhndirangu04@gmail.com>
2022-11-29 00:54:36 +0300

    feat:create a runner method for background jobs

--- tickets/apps.py
+++ tickets/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class TicketsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tickets'
+
+    def ready(self):
+        """override default ready method, to call background task when app starts"""
+        from jobs import updater
+        updater.start()
