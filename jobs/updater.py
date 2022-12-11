fd2107a58b9ea96857667b53b6d38e48af7e98f2
Willbeckh <wileyhndirangu04@gmail.com>
2022-12-11 20:28:04 +0300

    patch: interval time=15mins

--- jobs/updater.py
+++ jobs/updater.py
@@ -7,5 +7,5 @@ def start():
     """This method, creates a background schedule instance and uses it to schedule tasks within intervals"""
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_api, 'interval', minutes=45)
+    scheduler.add_job(schedule_api, 'interval', minutes=15)
     scheduler.start()
