9ec4932a6b8cc07e6e5def27d9b5e0540f16b30a
Willbeckh <wileyhndirangu04@gmail.com>
2022-12-12 02:55:32 +0300

    fix:app mode in db settings

--- ticketapi/settings.py
+++ ticketapi/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'ticketapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if config(APP_MODE) == 'dev':
+if config('APP_MODE') == 'dev':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
