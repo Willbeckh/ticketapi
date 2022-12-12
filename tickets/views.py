80a37e913d6a3022210a0b4f2f578dd643126e15
Willbeckh <wileyhndirangu04@gmail.com>
2022-12-12 16:15:13 +0300

    patch:add class docstrings

--- tickets/views.py
+++ tickets/views.py
@@ -9,32 +9,39 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from drf_yasg.generators import OpenAPISchemaGenerator
 
+
 # healthchecks ping url
 PING_URL = 'https://hc-ping.com/e238b07e-fa0b-425e-8622-d3f12a959b36'
 
 
-# Create your views here.        
+# Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
+    """Provides HTTP methods to retrive, update, soft delete a user object."""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    
-    
+
+
 class TicketViewSet(viewsets.ModelViewSet):
+    """Provides HTTP methods to retrive, update, soft delete a ticket object."""
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-    permission_classes = [IsAuthenticated] # require token authentication.
-    
+    permission_classes = [IsAuthenticated]  # require token authentication.
+
+
 class CategoryViewSet(viewsets.ModelViewSet):
+    """Provides HTTP methods to retrive, update, soft delete a category object."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
-    
-    
+
+
 class MyTokenObtainPairView(TokenObtainPairView):
     permission_classes = [AllowAny]
     serializer_class = MyTokenObtainPairSerializer
-    
+
+
 # ping endpoint
 @api_view(['GET'])
 def ping(request):
@@ -47,18 +54,18 @@ def ping(request):
 
         # check the response
         if response.status_code == 200:
-            return Response({ "response" : "Ping Successful!"})
+            return Response({"response": "Ping Successful!"})
         else:
             return Response(f"Error sending ping: {response.text}")
     except Exception as e:
         print(f"Error sending ping: {str(e)}")
-        
-        
+
+
 # swagger schemes generator class
 class CustomSchemaGenerator(OpenAPISchemaGenerator):
     """This class overrides the swagger default schema generator class."""
+
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
         schema.schemes = ["http", "https"]
         return schema
-    
