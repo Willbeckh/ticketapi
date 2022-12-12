1b65cbffa0876d799cdbef560e003829dc31783a
Willbeckh <wileyhndirangu04@gmail.com>
2022-12-12 16:11:54 +0300

    add class description

--- auth/views.py
+++ auth/views.py
@@ -3,7 +3,10 @@ from .serializers import Register
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
 
+
 class RegisterView(generics.CreateAPIView):
+    """Endpoint to create new users."""
+
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = Register
