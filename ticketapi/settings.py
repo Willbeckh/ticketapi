bfec691370bb8d0e6fb912af34bb06bcb62a80cb
Willbeckh <wileyhndirangu04@gmail.com>
2022-12-21 15:46:10 +0300

    fix: change app mode == test

--- ticketapi/settings.py
+++ ticketapi/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY',  default='try-!1-a--_guessing_g4m3#$%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True)
-APP_MODE = config('APP_MODE', default='dev')
+APP_MODE = config('APP_MODE', default='test')
 
 ALLOWED_HOSTS = ['*']
 
